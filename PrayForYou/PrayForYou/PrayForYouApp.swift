0bc036b019428f6bbb592bfe9c3010b944c60005
Christoph Wottawa <christoph.wottawa@me.com>
2023-12-02 18:51:55 +0100

    Setup Xcode Project

---
+++ PrayForYou/PrayForYou/PrayForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayForYouApp.swift
+//  PrayForYou
+//
+//  Created by Christoph Wottawa on 02.12.23.
+//
+
+import SwiftUI
+
+@main
+struct PrayForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
